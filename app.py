77c21b5ccdb71fd227d73fd3e1f0a1552c8906bd
Pedro Correia Cravo Pimenta <pedroccpimenta@gmail.com>
2023-08-16 16:51:47 +0100

    Update app.py
    tuning

--- app.py
+++ app.py
@@ -112,5 +112,5 @@ def update_scattergraph(site_dropdown,payload_slider):
 
 
 
-if __name__ == '__main__':
-    app.run_server(debug=False)
+#if __name__ == '__main__':
+#    app.run_server(debug=False)
